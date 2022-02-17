d489b3d3de7f7aa98fa5010944b3db32c5e77c88
vagrant <tpirozzini@gmail.com>
2022-02-17 00:50:36 +0000

    add exit handler

--- client.js
+++ client.js
@@ -12,15 +12,36 @@ const connect = function () {
     console.log("New Player connected to Game Server");
   });
 
-  // name your snake
+  // name your snake && move: up
   conn.on("connect", () => {
-    conn.write(`Name: TSP`);
+    conn.write("Name: TEE");    
+    // conn.write("Move: up");    
   });
 
   // handle incoming data
   conn.on("data", (data) => {
-    console.log(data);
+    console.log("Server: ", data);
   });  
+
+  // move up: with setTimeout
+  // let delay = 0;
+  // let count = 100;
+  // for (i = 0; i <= count; i++){
+  //   conn.on("connect", (data) => {
+  //     setTimeout(() => {
+  //       conn.write(`Move: up`)}, delay += 50);        
+  //   });
+  // };
+
+  // move up: with setInterval
+  // let delay = 0;
+  // let count = 10;
+  // for (i = 0; i <= count; i++){
+  //   conn.on("connect", (data) => {
+  //     setInterval(() => {
+  //       conn.write(`Move: up`)}, delay += 50);        
+  //   });
+  // };
 
   // interpret incoming data as text
   conn.setEncoding("utf8");  
--- play.js
+++ play.js
@@ -2,3 +2,20 @@ const net = require("net");
 const connect = require("./client");
 
 console.log("connecting ...");
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function () {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+};
+
+setupInput();
