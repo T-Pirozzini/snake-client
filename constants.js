4489ccca40c271d0ff5e39baa97232a791444ab3
vagrant <tpirozzini@gmail.com>
2022-02-17 01:46:01 +0000

    add constants

---
+++ constants.js
@@ -0,0 +1,23 @@
+const IP = "165.227.47.243";
+const PORT = 50541;
+
+// Movement
+const MOVE_UP = 'w';
+const MOVE_RIGHT = 'd';
+const MOVE_DOWN = 's';
+const MOVE_LEFT = 'a';
+
+// Taunts
+const TAUNT_1 = 'p';
+const TAUNT_2 = 'o';
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP,
+  MOVE_RIGHT,
+  MOVE_DOWN,
+  MOVE_LEFT,
+  TAUNT_1,
+  TAUNT_2
+};
--- input.js
+++ input.js
@@ -1,15 +1,41 @@
-const setupInput = function () {
+const { MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT, TAUNT_1,TAUNT_2 } = require("./constants");
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  const handleUserInput = function () {
+
+  
+  const handleUserInput = key => {
     if (key === '\u0003') {
       process.exit();
-    }
+    };
+    if (key === MOVE_UP) {
+      conn.write("Move: up");
+    };
+    if (key === MOVE_RIGHT) {
+      conn.write("Move: right");
+    };
+    if (key === MOVE_DOWN) {
+      conn.write("Move: down");
+    };
+    if (key === MOVE_LEFT) {
+      conn.write("Move: left");
+    };
+    if (key === TAUNT_1) {
+      conn.write("Say: I'm soooo fast!!");
+    };
+    if (key === TAUNT_2) {
+      conn.write("Say: Bring it on!!");
+    };
   };
-  stdin.on("data", handleUserInput);
-  
+    
+  stdin.on("data", (data) => {
+    handleUserInput(data);
+  });  
   return stdin;
 };
 
